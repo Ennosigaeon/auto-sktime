0c78fa5a1a0f1badb87f0cd35b0e9da78d6a0def
Marc Zoeller <m.zoeller@usu.de>
2022-04-03 10:18:34 +0200

    Add naive forecasting component

---
+++ autosktime/pipeline/components/forecast/naive.py
@@ -0,0 +1,54 @@
+from typing import Optional
+
+from sktime.forecasting.base import ForecastingHorizon
+
+from ConfigSpace import ConfigurationSpace, CategoricalHyperparameter
+from autosktime.pipeline.components.base import AutoSktimeComponent, AutoSktimePredictor
+
+
+class NaiveForecasterComponent(AutoSktimeComponent, AutoSktimePredictor):
+
+    def __init__(
+            self,
+            sp: int = 1,
+            strategy: str = 'last',
+    ):
+        self.sp = sp
+        self.strategy = strategy
+
+    def fit(self, y, X=None, fh: Optional[ForecastingHorizon] = None):
+        from sktime.forecasting.naive import NaiveForecaster
+        self.estimator = NaiveForecaster(
+            sp=self.sp,
+            strategy=self.strategy,
+        )
+        self.estimator.fit(y, X=X, fh=fh)
+        return self
+
+    def predict(
+            self,
+            fh: Optional[ForecastingHorizon] = None,
+            X=None
+    ):
+        prediction = super().predict(fh, X)
+
+        if self.sp > 1 and fh is not None and self.estimator.fh[0] == fh.to_pandas()[0]:
+            # NaiveForecaster uses the last self.sp terms for forecasting. In case that the training
+            # data are predicted again, the first self.sp terms are missing and set to nan
+            prediction = prediction.backfill()
+
+        return prediction
+
+    @staticmethod
+    def get_properties(dataset_properties=None):
+        from sktime.forecasting.naive import NaiveForecaster
+        return NaiveForecaster.get_class_tags()
+
+    @staticmethod
+    def get_hyperparameter_search_space(dataset_properties=None):
+        strategy = CategoricalHyperparameter('strategy', ['last', 'mean', 'drift'])
+        sp = CategoricalHyperparameter('sp', [1, 2, 4, 7, 12, 24])
+
+        cs = ConfigurationSpace()
+        cs.add_hyperparameters([strategy, sp])
+        return cs
