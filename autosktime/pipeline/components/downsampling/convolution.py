1b5e53e1f1f7f4a4e0832da928f5326855d1e152
marc <marc.zoeller@usu.com>
2022-09-15 17:53:02 +0200

    Set self.window_size_

--- autosktime/pipeline/components/downsampling/convolution.py
+++ autosktime/pipeline/components/downsampling/convolution.py
@@ -22,14 +22,14 @@ class ConvolutionDownSampler(BaseDownSampling):
 
     def _transform(self, X: Union[pd.Series, pd.DataFrame], y: pd.DataFrame = None):
         if isinstance(self.window_size, float):
-            n = int(X.shape[0] * self.window_size)
+            self.window_size_ = int(X.shape[0] * self.window_size)
         else:
-            n = int(self.window_size)
+            self.window_size_ = int(self.window_size)
 
         self._original_size = X.shape[0]
-        self._X_filter = np.ones((n, X.shape[1])) / n
+        self._X_filter = np.ones((self.window_size_, X.shape[1])) / self.window_size_
 
-        Xt = signal.convolve(X, self._X_filter, mode='valid')[::n]
+        Xt = signal.convolve(X, self._X_filter, mode='valid')[::self.window_size_]
 
         index = X.index
         if isinstance(index, pd.PeriodIndex):
@@ -39,8 +39,8 @@ class ConvolutionDownSampler(BaseDownSampling):
 
         Xt = pd.DataFrame(Xt, columns=X.columns, index=index)
         if y is not None:
-            self._y_filter = np.ones((n, y.shape[1])) / n
-            yt = pd.DataFrame(signal.convolve(y, self._y_filter, mode='valid')[::n], columns=y.columns, index=index)
+            self._y_filter = np.ones((self.window_size_, y.shape[1])) / self.window_size_
+            yt = pd.DataFrame(signal.convolve(y, self._y_filter, mode='valid')[::self.window_size_], columns=y.columns, index=index)
         else:
             yt = None
 
