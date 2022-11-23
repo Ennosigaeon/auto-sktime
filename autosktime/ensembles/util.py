27334aa0e95e4e0ee1d131429e84089915583e48
marc <marc.zoeller@usu.com>
2022-11-23 22:41:53 +0100

    Make ensemble forecaster more robust against single failures

--- autosktime/ensembles/util.py
+++ autosktime/ensembles/util.py
@@ -1,9 +1,9 @@
-from typing import Optional, List
-
+import logging
 import numpy as np
 import pandas as pd
 from sktime.forecasting.compose import EnsembleForecaster
 from sktime.forecasting.compose._ensemble import _check_aggfunc
+from typing import Optional, List, Tuple
 
 from autosktime.constants import SUPPORTED_Y_TYPES
 from autosktime.pipeline.components.base import AutoSktimePredictor
@@ -13,14 +13,40 @@ from autosktime.pipeline.util import NotVectorizedMixin
 # noinspection PyAbstractClass
 class PrefittedEnsembleForecaster(NotVectorizedMixin, EnsembleForecaster, AutoSktimePredictor):
 
+    def __init__(
+            self,
+            forecasters: List[AutoSktimePredictor],
+            n_jobs: int = None,
+            aggfunc: str = "mean",
+            weights: List[float] = None
+    ):
+        super().__init__(forecasters, n_jobs=n_jobs, aggfunc=aggfunc, weights=weights)
+        self.logger = logging.getLogger(__name__)
+
     def _fit(self, y, X=None, fh=None):
         self.forecasters_ = self.forecasters
 
     def _predict(self, fh, X=None):
-        y_pred = pd.concat(self._predict_forecasters(fh, X), axis=1)
-        y_pred = _aggregate(y=y_pred, aggfunc=self.aggfunc, weights=self.weights)
+        y_pred, valid = self._predict_forecasters(fh, X)
+        weights = [self.weights[i] for i in valid] if self.weights is not None else None
+        y_pred = _aggregate(y=y_pred, aggfunc=self.aggfunc, weights=weights)
 
         return y_pred
+
+    def _predict_forecasters(self, fh=None, X=None) -> Tuple[pd.Series, List[int]]:
+        res = []
+        valid = []
+
+        for i, forecaster in enumerate(self.forecasters_):
+            try:
+                res.append(forecaster.predict(fh=fh, X=X))
+                valid.append(i)
+            except KeyboardInterrupt:
+                raise
+            except Exception:
+                self.logger.warning(f'Failed to create predictions with forecaster {forecaster}:', exc_info=True)
+
+        return pd.concat(res, axis=1), valid
 
 
 def _aggregate(y: SUPPORTED_Y_TYPES, aggfunc: str, weights: Optional[List[float]]) -> SUPPORTED_Y_TYPES:
