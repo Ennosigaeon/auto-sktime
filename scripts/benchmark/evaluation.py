6f011efc60699d6b7447f44e38cb4f8c16f26258
usimzoller <marc.zoeller@usu.com>
2023-12-13 16:22:37 +0100

    Rework evaluation script

--- scripts/benchmark/evaluation.py
+++ scripts/benchmark/evaluation.py
@@ -9,113 +9,127 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', 10000)
 pd.set_option("display.precision", 2)
+pd.set_option('use_inf_as_na', True)
 
 
-def analyse_results(with_timeout: bool = True, with_missing_values: bool = True):
+def analyse_results(
+        with_timeout: bool = True,
+        with_missing_values: bool = True,
+        imputation_method: str = 'worst',
+        metric: str = 'mase',
+        max_duration: int = 360
+):
     print(f'with_timeout = {with_timeout}, with_missing_values = {with_missing_values}')
 
-    for time in (300, 60):
-        benchmark = {
-            'auto-pytorch': pd.read_csv(f'results/auto-pytorch-{time}.csv'),
-            'auto-sktime': pd.read_csv(f'results/auto-sktime-{time}.csv'),
-            'autogluon': pd.read_csv('results/autogluon.csv'),
-            # 'autogluon_hpo': pd.read_csv('results/autogluon.hpo.csv'),
-            # 'autots_default': pd.read_csv(f'results/autots-{time}.default.csv'),
-            'autots': pd.read_csv(f'results/autots-{time}.csv'),
-            'hyperts': pd.read_csv(f'results/hyperts-{time}.csv'),
-            'pmdarima': pd.read_csv('results/pmdarima.csv'),
-            # 'prophet': pd.read_csv('results/prophet.csv'),
-            'pyaf': pd.read_csv('results/pyaf.csv'),
-            'tft': pd.read_csv(f'results/tft-{time}.csv')
-        }
-        ablation_study = {
-            'auto-sktime': pd.read_csv(f'results/auto-sktime-{time}.csv'),
-            'auto-sktime_templates': pd.read_csv(f'results/auto-sktime_templates-{time}.csv'),
-            'auto-sktime_multi_fidelity': pd.read_csv(f'results/auto-sktime_multi_fidelity-{time}.csv'),
-            'auto-sktime_warm_starting': pd.read_csv(f'results/auto-sktime_warm_starting-{time}.csv'),
-            'auto-pytorch': pd.read_csv(f'results/auto-pytorch-{time}.csv'),
-        }
+    benchmark = {
+        'auto-pytorch': pd.read_csv(f'results2/auto-pytorch/_result.csv'),
+        'auto-sktime': pd.read_csv(f'results2/auto-sktime/_result.csv'),
+        'autogluon': pd.read_csv('results2/autogluon/_result.csv'),
+        'autots': pd.read_csv(f'results2/autots_random/_result.csv'),
+        'hyperts': pd.read_csv(f'results2/hyperts/_result.csv'),
+        'pmdarima': pd.read_csv('results2/pmdarima/_result.csv'),
+        'pyaf': pd.read_csv('results2/pyaf/_result.csv'),
+        'naive': pd.read_csv('results2/naive/_result.csv'),
+        'ets': pd.read_csv('results2/ets/_result.csv'),
+        'tft': pd.read_csv(f'results2/tft/_result.csv'),
+        'deepar': pd.read_csv(f'results2/deepar/_result.csv'),
+    }
+    ablation_study = {
+        'auto-sktime': pd.read_csv(f'results2/auto-sktime/_result.csv'),
+        'auto-sktime_templates': pd.read_csv(f'results2/auto-sktime_templates/_result.csv'),
+        'auto-sktime_multi_fidelity': pd.read_csv(f'results2/auto-sktime_multi_fidelity/_result.csv'),
+        'auto-sktime_warm_starting': pd.read_csv(f'results2/auto-sktime_warm_starting/_result.csv'),
+        'auto-pytorch': pd.read_csv(f'results2/auto-pytorch/_result.csv'),
+        'naive': pd.read_csv('results2/naive/_result.csv'),
+    }
 
-        for results in (ablation_study, benchmark):
-            print(f'###############\n'
-                  f'Benchmark {results == benchmark}, Duration {time}, Timeout {with_timeout}, Missing Values {with_missing_values}\n'
-                  f'###############\n')
+    for results in (ablation_study, benchmark):
+        print(f'###############\n'
+              f'Benchmark {results == benchmark}, Timeout {with_timeout}, Missing Values {with_missing_values}\n'
+              f'###############\n')
 
-            df = pd.concat(results.values())
+        df = pd.concat(results.values())
 
-            if not with_missing_values:
-                nan = df[(df['method'] == 'pmdarima') & np.isinf(df['smape'])]['dataset'].unique()
-                df = df[~df['dataset'].isin(nan)]
+        if not with_missing_values:
+            nan = df[(df['method'] == 'pmdarima') & np.isinf(df[metric])]['dataset'].unique()
+            df = df[~df['dataset'].isin(nan)]
 
-            if with_timeout:
-                df.loc[(df['duration'] > time + 60), 'smape'] = np.inf
-            df = df.drop(columns=['seed']).replace([np.inf], 2).round(5)
+        if with_timeout:
+            df.loc[(df['duration'] > max_duration), metric] = np.inf
 
-            print('Number of timeouts')
-            print(df.loc[(df['duration'] > time + 60), 'method'].value_counts())
+        df = df.drop(columns=['seed']).round(5)
+        if imputation_method == 'worst':
+            missing = pd.isna(df).any(axis=1)
+            worst = (1.05 * df[['dataset', metric]].dropna().groupby('dataset').max()).loc[df.loc[missing, 'dataset']]
+            worst.index = df.loc[missing, metric].index
+            df.loc[missing, metric] = worst
+        elif imputation_method == 'naive':
+            imputation = df[df['method'] == 'naive']
+            for method in df['method'].unique():
+                missing = df[df['method'] == method][metric] == np.inf
+                df.loc[(df['method'] == method) & (df[metric] == np.inf), metric] = imputation[missing][metric]
 
-            if with_timeout:
-                df['duration'] = df['duration'].clip(upper=time + 60)
+        df = df[df['method'] != 'naive']
 
-            rank = df.groupby(['dataset', 'method']).mean(numeric_only=True).groupby('dataset').rank()[
-                'smape'].reset_index().rename(columns={'smape': 'performance_rank'})
-            df = pd.merge(df, rank, how='left', left_on=['method', 'dataset'], right_on=['method', 'dataset'])
+        print('Number of timeouts')
+        print(df.loc[(df['duration'] > max_duration), 'method'].value_counts())
 
-            for column in ('smape', 'duration', 'performance_rank'):
-                best = df[['method', column]].groupby('method').mean()[column].idxmin()
+        if with_timeout:
+            df['duration'] = df['duration'].clip(upper=max_duration)
 
-                for method in df['method'].unique():
-                    t = ttest_ind(
-                        df[df['method'] == best][column].values,
-                        df[df['method'] == method][column].values
-                    )
-                    if t.pvalue * len(df['method'].unique()) > 0.05:
-                        print(column, method, t)
-            raw = df.copy()
+        rank = df.groupby(['dataset', 'method']).mean(numeric_only=True).groupby('dataset').rank()[
+            metric].reset_index().rename(columns={metric: 'performance_rank'})
+        df = pd.merge(df, rank, how='left', left_on=['method', 'dataset'], right_on=['method', 'dataset'])
 
-            df = df.groupby(['dataset', 'method']).mean(numeric_only=True).reset_index()
-            aggregated = df.groupby('method').mean(numeric_only=True).join(
-                df.groupby('method').std(numeric_only=True),
-                lsuffix='_mean',
-                rsuffix='_std'
-            )
-            print(aggregated.sort_index(key=lambda col: col.str.lower()), end='\n\n\n')
+        for column in (metric, 'duration', 'performance_rank'):
+            best = df[['method', column]].groupby('method').mean()[column].idxmin()
 
-            wide = df[['dataset', 'method', 'smape']].pivot(index='dataset', columns='method', values='smape')
-            diagram = Diagram(
-                wide.to_numpy(),
-                treatment_names=wide.columns,
-                maximize_outcome=False
-            )
-            filename = f'cd_{"benchmark" if results == benchmark else "ablation"}-{time}{"_timeout" if with_timeout else ""}{"_nan" if with_missing_values else ""}.pdf'
-            # Patch calculated p values to use same method as used for tabular significance test
-            for i, j in np.argwhere(np.isfinite(diagram.P)):
-                diagram.P[i, j] = ttest_ind(
-                    raw[raw['method'] == diagram.treatment_names[i]]['smape'].values,
-                    raw[raw['method'] == diagram.treatment_names[j]]['smape'].values
-                ).pvalue * len(df['method'].unique()) / np.cumsum(np.arange(1, len(df['method'].unique())))[-1]
+            for method in df['method'].unique():
+                t = ttest_ind(
+                    df[df['method'] == best][column].values,
+                    df[df['method'] == method][column].values
+                )
+                if t.pvalue * len(df['method'].unique()) > 0.05:
+                    print(column, method, t)
+        raw = df.copy()
 
-            diagram.to_file(filename, alpha=.05, adjustment='bonferroni', reverse_x=False)
-            for suffix in ('.aux', '.log', '.tex'):
-                os.remove(filename.replace('.pdf', suffix))
+        df = df.groupby(['dataset', 'method']).mean(numeric_only=True).reset_index()
+        aggregated = df.groupby('method').mean(numeric_only=True).join(
+            df.groupby('method').std(numeric_only=True),
+            lsuffix='_mean',
+            rsuffix='_std'
+        )
+        print(aggregated.sort_index(key=lambda col: col.str.lower()), end='\n\n\n')
 
-            raw = df.pivot_table(index='dataset', columns='method', values=['smape', 'duration', 'performance_rank'],
-                                 aggfunc='first')
+        wide = df[['dataset', 'method', metric]].pivot(index='dataset', columns='method', values=metric)
+        diagram = Diagram(
+            wide.to_numpy(),
+            treatment_names=wide.columns,
+            maximize_outcome=False
+        )
+        filename = f'cd_{"benchmark" if results == benchmark else "ablation"}{"_timeout" if with_timeout else ""}{"_nan" if with_missing_values else ""}.pdf'
+        diagram.to_file(filename, alpha=.05, adjustment='bonferroni', reverse_x=False)
+        for suffix in ('.aux', '.log', '.tex'):
+            os.remove(filename.replace('.pdf', suffix))
 
-            methods = df['method'].unique()
-            print(methods)
-            for index, row in raw.iterrows():
-                print(index.replace('_', '\\_'), end='')
-                for method in methods:
-                    print(
-                        ' & '
-                        f'{row[("smape", method)]:.2f}', ' & ',
-                        # f'{row[("performance_rank", method)]:.2f}', ' & ',
-                        f'{row[("duration", method)]:.2f}',
-                        end=' '
-                    )
-                print('\\\\')
-            pass
+        raw = df.pivot_table(index='dataset', columns='method', values=[metric, 'duration', 'performance_rank'],
+                             aggfunc='first')
+        raw.round(2).to_csv('raw.csv')
+
+        methods = df['method'].unique()
+        print(methods)
+        for index, row in raw.iterrows():
+            print(index.replace('_', '\\_')[:-4], end='')
+            for method in methods:
+                print(
+                    ' & '
+                    f'{row[(metric, method)]:.2f}', ' & ',
+                    # f'{row[("performance_rank", method)]:.2f}', ' & ',
+                    f'{int(row[("duration", method)])}',
+                    end=' '
+                )
+            print('\\\\')
+        pass
 
 
 analyse_results(with_timeout=True, with_missing_values=True)
